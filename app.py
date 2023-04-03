e7321755a7b5ee25f5ac70e1636c3095acba4f60
Prince Kurien <pk1308@users.noreply.huggingface.co>
2023-04-03 05:35:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ with gr.Blocks() as demo:
                                       PAY_3 , PAY_4 , PAY_5 , PAY_6 , BILL_AMT1 , BILL_AMT2 , BILL_AMT3 , BILL_AMT4 , BILL_AMT5 , 
                                       BILL_AMT6 , PAY_AMT1 , PAY_AMT2 , PAY_AMT3 , PAY_AMT4 , PAY_AMT5 , PAY_AMT6],outputs=[label])
 
-demo.launch(share=True)
+demo.launch()
 
 
 
